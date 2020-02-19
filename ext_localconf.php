190719d62d0ee898f2f57487f412fa5a52b12c9a
Sybille Peters <sypets@gmx.de>
2020-02-19 13:47:44 +0100

    Do not use use in ext_localconf.php
    Use fully qualified class name.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,7 @@
 defined('TYPO3_MODE') or die();
 
 (function () {
-    use TYPO3\CMS\Core\Utility\GeneralUtility;
-    use TYPO3\CMS\Core\Locking\LockFactory;
-    use B13\DistributedLocks\RedisLockingStrategy;
-
-    $lockFactory = GeneralUtility::makeInstance(LockFactory::class);
-    $lockFactory->addLockingStrategy(RedisLockingStrategy::class);
+    $lockFactory = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Locking\LockFactory::class);
+    $lockFactory->addLockingStrategy(\B13\DistributedLocks\RedisLockingStrategy::class);
 })();
 
