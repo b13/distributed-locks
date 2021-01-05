c06ed7e9ef973f83df3ff472190e63beaf97f481
Benni Mack <benni@typo3.org>
2021-01-05 08:43:51 +0100

    [TASK] Add support for TYPO3 v10/v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'b13 GmbH',
     'constraints' => [
         'depends' => [
-                'typo3' => '8.7.0-9.9.99',
+                'typo3' => '8.7.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
