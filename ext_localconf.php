5d378446a2e842f356f7166b86aa2961c8055de7
Ernesto Baschny <eb@cron.eu>
2022-01-21 12:47:01 +0100

    Fixed boolean condition for 'disabled'

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@
 defined('TYPO3_MODE') or die();
 
 (function () {
-    if (!empty($GLOBALS['TYPO3_CONF_VARS']['SYS']['locking']['redis']['disabled'])) {
+    if (empty($GLOBALS['TYPO3_CONF_VARS']['SYS']['locking']['redis']['disabled'])) {
         $lockFactory = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Locking\LockFactory::class);
         $lockFactory->addLockingStrategy(\B13\DistributedLocks\RedisLockingStrategy::class);
     }
