526f3f955f8648c145a0b15774cbc6cde9272d8a
Stephan Großberndt <s.grossberndt@sidebysite.de>
2022-12-12 17:11:19 +0100

    [TASK] Add RedisLockingStrategy to TYPO3_CONF_VARS-SYS-locking-strategies (#21)

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,8 +4,8 @@ defined('TYPO3_MODE') or die();
 
 (function () {
     if (empty($GLOBALS['TYPO3_CONF_VARS']['SYS']['locking']['redis']['disabled'] ?? false)) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['locking']['strategies'][\B13\DistributedLocks\RedisLockingStrategy::class] = [];
         $lockFactory = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Locking\LockFactory::class);
         $lockFactory->addLockingStrategy(\B13\DistributedLocks\RedisLockingStrategy::class);
     }
 })();
-
